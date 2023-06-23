f2e13c05b095ed30f462e51ac45a38f592087874
Omar <oromero@unsa.edu.pe>
2023-06-23 12:12:30 -0500

    aumentando en campo donador

--- src/personas/models.py
+++ src/personas/models.py
@@ -5,3 +5,4 @@ class Persona(models.Model):
     nombres = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
     edad = models.IntegerField()
+    donador = models.BooleanField()
